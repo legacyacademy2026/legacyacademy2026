5942bad3d62897dd22651e88536472028fa510c3
Aruna <janakaantha@gmail.com>
2026-06-24 19:07:17 +0400

    Switch to port 587 with STARTTLS for Gmail SMTP reliability

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
